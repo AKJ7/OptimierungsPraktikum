6bfd12a8975e36d6674aee850dba337044b952bc
AKJ7 <akj123429@gmail.com>
2018-11-18 12:47:27 +0100

    Mistake in KreisFinding function detected

--- test3.m
+++ test3.m
@@ -5,6 +5,8 @@
 %A = [0 1 0 0; 1 0 1 0; 0 1 0 1; 0 0 1 0];
 %A = [0 1 1 1 1 0 0; 1 0 1 0 0 1 0; 1 1 0 0 1 1 0; 1 0 0 0 1 0 1; 1 0 1 1 0 1 1; 0 1 1 0 1 0 1; 0 0 0 1 1 1 0];
 %A = [0 1 0 0 1; 1 0 1 0 0; 0 1 0 1 0; 0 0 1 0 1; 1 0 0 1 0];
+
+A = [0 1 0 1 0 0; 1 0 1 0 1 0; 0 1 0 0 1 0; 1 0 0 0 0 1; 0 1 1 0 0 0; 0 0 0 1 0 0];
 a = 1;
 B = {};
 while a <= length(A)
@@ -20,36 +22,43 @@ end
 
 C = B;
 
-D = kreisCheck({[5 7]  [5 6]  [1 2]  [1 4]  [1 3]  [3 5] [4 7]});
+%D = kreisCheck({[1 4]; [3 5]; [4 6]; [1 2]; [2 5]; [2 3]});
+D = kreisCheck(C, iteration);
 if (D{1}(1) == D{1}(2))
     disp(1);
 else
     disp(0);
 end
+iteration = 1;
 
-function output = kreisCheck(kanten)
+function output = kreisCheck(kanten, iter)
     current = 1;
     counter = 1;
-    
+    disp(iter);
     while counter <= length(kanten)
         if counter ~= current
             if kanten{current}(2) == kanten{counter}(1)
                 kanten{current} = [kanten{current}(1), kanten{counter}(2)];
                 kanten(counter) = [];
+                disp('First if');
                 if (length(kanten) == 1)
                     output = kanten;
                     return;
                 else
-                    kanten = kreisCheck(kanten);
+                    disp('First if Else');
+                    kanten = kreisCheck(kanten, iter + 1);
+                    
                 end
             elseif kanten{current}(2) == kanten{counter}(2)
                 kanten{current} = [kanten{current}(1), kanten{counter}(1)];
                 kanten(counter) = [];
+                disp('Second if');
                 if (length(kanten) == 1)
                     output = kanten;
                     return;
                 else
-                    kanten = kreisCheck(kanten);
+                    disp('Second if else');
+                    kanten = kreisCheck(kanten, iter + 1);
                 end
             end
         end
